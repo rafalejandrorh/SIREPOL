6f5048d7ff0851a7cf93052060cf35020d497e00
Rafael Rivero <rafalejandrorivero@gmail.com>
2022-08-11 16:18:51 -0400

    Update 11.08.22

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,9 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        $roles = Role::pluck('name','name')->all();
+        
+        return view('users.create',compact('roles'));
     }
 
     /**
